09559111cbc8689b3e57db6789102d6dafa66dad
Yash Vardhan <yashvardhan513@gmail.com>
2021-05-21 17:12:30 +0530

    add the contact component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import ButtonComponent from 'components/ButtonComponent'
+import {ButtonComponent} from 'components/ButtonComponent'
+import {ContactComponent} from 'components/ContactComponent'
 
 export const App = ({}) => {
     return (
+      <div>
       <ButtonComponent heading="Button Component" buttonText="Button" />
+      <ContactComponent contactMessage="Contact us" subscribeMessage="Subscribe to Newsletter" />
+      </div>
     )
 }
