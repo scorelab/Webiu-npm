a2e4e1ba301ff6326785eb4cfa4fe514ece1f1f0
Yash Vardhan <yashvardhan513@gmail.com>
2021-07-17 17:35:48 +0530

    add about component module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {ButtonComponent} from 'components/ButtonComponent'
 import {ContactComponent} from 'components/ContactComponent'
+import { AboutComponent } from 'components/AboutComponent/AboutComponent'
 
 export const App = () => {
     return (
       <div>
         <ButtonComponent buttonText="Button" buttonWidth="400px" />
         <ContactComponent contactTitle="Contact us" subscribeTitle="Subscribe to News" contactMessage="Contact us" subscribeMessage="Subscribe to Newsletter" />
+        <AboutComponent header="About Component" mainText="Welcome to SCoRe Lab" subText="This is a description text" buttonText="Explore" buttonLink="/" image="https://scorelab.org/images/community.png" />
       </div>
     )
 }
