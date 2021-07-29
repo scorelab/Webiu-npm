70ef42970b00588421be852de4f0fb5a9182cc44
Yash Vardhan <yashvardhan513@gmail.com>
2021-07-29 12:38:21 +0530

    modify all components and fix minor bugs

--- src/App.js
+++ src/App.js
@@ -52,16 +52,12 @@ import { Typewriter } from 'components/Typewriter'
 export const App = () => {
     return (
       <div>
-        <ButtonComponent buttonText="Button" heading="Button Component" buttonWidth="350px" />
+        <HeaderComponent mainText="WELCOME TO SCoRe LAB" 
+                         subText="The SCoRe Lab has conducted research covering various aspects of sensor networks, embeded systems, digital forensic, information security, mobile applications, cloud, blockchain and software tools." 
+                         buttonText="Explore" buttonLink="/"
+                         image="https://scorelab.org/images/community.png" small={false} />
 
-        <ContactComponent contactTitle="Contact us" subscribeTitle="Subscribe to News" contactMessage="Contact us" 
-                          subscribeMessage="Subscribe to Newsletter" />
-
-        <AboutComponent header="About Component" mainText="Welcome to SCoRe Lab" subText="This is a description text" 
-                        buttonText="Explore" buttonLink="/" image="https://scorelab.org/images/community.png" />
-
-        <AnnouncementComponent header="Announcements" speed="2" title="Announcements" direction="up" 
-                               data={[{text: "Notice 1", link: "/"}, {text: "Notice 2", link: "/"}]} />
+        <SignupAndLogin signupMessage="Signup" loginMessage="Login" handleSignup={() => {}} handleLogin={() => {}} />
 
         <CardComponent header="Services Component" card={false} services={true} buttonText="All Services" buttonLink="/"
                        data={[{image: "https://uxwing.com/wp-content/themes/uxwing/download/07-web-app-development/software-engineer.svg", 
@@ -74,6 +70,29 @@ export const App = () => {
                                title: "ANALYTICS", 
                                description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution"}]} />
 
+        <CarouselComponent data={[{title: "LabelLab", content: "Lorem ipsum sample data", 
+                                   image: "https://scorelab.org/logos/projects/labellab.png"},
+                                  {title: "Dronesym", content: "Lorem ipsum sample data", 
+                                   image: "https://scorelab.org/logos/projects/dronesym.png"},
+                                  {title: "Senz", content: "Lorem ipsum sample data", 
+                                   image: "https://scorelab.org/logos/projects/senz.png"}]}
+                           header="Carousel" />
+
+        <ComingsoonComponent header="Countdown" image="https://scorelab.org/images/joinTeam.png" buttonLink="/" 
+                             countdownText="Coming soon!" deployDate="1 Jan, 2022 00:00:00" buttonText="Explore" 
+                             message="The new version of this package is coming soon!" />
+
+        <ButtonComponent buttonText="Button" heading="Button Component" buttonWidth="350px" />
+
+        <ContactComponent contactTitle="Contact us" subscribeTitle="Subscribe to News" contactMessage="Contact us" 
+                          subscribeMessage="Subscribe to Newsletter" />
+
+        <AboutComponent header="About Component" mainText="Welcome to SCoRe Lab" subText="This is a description text" 
+                        buttonText="Explore" buttonLink="/" image="https://scorelab.org/images/community.png" />
+
+        <AnnouncementComponent header="Announcements" speed="2" title="Announcements" direction="up" 
+                               data={[{text: "Notice 1", link: "/"}, {text: "Notice 2", link: "/"}]} />
+
         <CardComponent header="Card Component" card={true} services={false}
                        data={[{image: "https://scorelab.org/logos/projects/dronesym.png", title: "Dronesym", 
                                description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution", 
@@ -84,18 +103,6 @@ export const App = () => {
                               {image: "https://scorelab.org/logos/projects/bassa.png", title: "Bassa", 
                                description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution", 
                                buttonText: "All Services", buttonLink: "/"}]} />  
-        
-        <ComingsoonComponent header="Countdown" image="https://scorelab.org/images/joinTeam.png" buttonLink="/" 
-                             countdownText="Coming soon!" deployDate="1 Jan, 2022 00:00:00" buttonText="Explore" 
-                             message="The new version of this package is coming soon!" />
-
-        <CarouselComponent data={[{title: "LabelLab", content: "Lorem ipsum sample data", 
-                                   image: "https://scorelab.org/logos/projects/labellab.png"},
-                                  {title: "Dronesym", content: "Lorem ipsum sample data", 
-                                   image: "https://scorelab.org/logos/projects/dronesym.png"},
-                                  {title: "Senz", content: "Lorem ipsum sample data", 
-                                   image: "https://scorelab.org/logos/projects/senz.png"}]}
-                                  header="Carousel" />
 
         <DescriptionComponent title="Description Container" 
                               subTitle="This component can be used to declare description regarding of a particular instance"
@@ -114,22 +121,12 @@ export const App = () => {
                               content: "Lorem ispum is a sample text which can be used to simply populate the data to check interface"},
                              {title: "Lorem ispum is a sample text which can be used to simply populate the data to check interface", 
                               content: "Lorem ispum is a sample text which can be used to simply populate the data to check interface"},]} />
-        
-        <FooterComponent socialLinks={{facebook: "/", gitter: "/", twitter: "/", github: "/", 
-                            linkedin:"/", medium:"/"}} mainText="Copyright © SCoRe Lab" />
-
-        <HeaderComponent mainText="WELCOME TO SCoRe LAB" 
-                         subText="The SCoRe Lab has conducted research covering various aspects of sensor networks, embeded systems, digital forensic, information security, mobile applications, cloud, blockchain and software tools." 
-                         buttonText="Explore" buttonLink="/"
-                         image="https://scorelab.org/images/community.png" small={false} />
 
         <LetterAvatar name="Yash Vardhan" bgColor="green" />
 
         <Partner header="Partner-With-Us" mainText="Collaborate with us" contactText="contact us" contactLink="/" 
                  buttonText="Form" buttonLink="/" image="https://scorelab.org/images/joinTeam.png" 
                  subText="The SCoRe Lab has conducted research covering various aspects of sensor networks, embeded systems, digital forensic, information security, mobile applications, cloud, blockchain and software tools." />
-
-        <SignupAndLogin signupMessage="Signup" loginMessage="Login" handleSignup={() => {}} handleLogin={() => {}} />
 
         <Pricing title="Subscription Charges" header="Pricing" description="sample-description"
                  plans={[{plan: "BASIC", description: "sample", price: 30, 
@@ -303,6 +300,9 @@ export const App = () => {
 
         <Typewriter header="Typewriter" wait={100} color="" staticText="I AM A"
                     words={["DEVELOPER", "PROGRAMMER", "DEBUGGER"]} />
+
+        <FooterComponent socialLinks={{facebook: "/", gitter: "/", twitter: "/", github: "/", 
+                                       linkedin:"/", medium:"/"}} mainText="Copyright © SCoRe Lab" />
 
       </div>
     )
